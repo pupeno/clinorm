bae9641c181bd9cb8e05a72a170e7865695cba36
J. Pablo Fernández <pupeno@pupeno.com>
2008-11-27 23:12:30 +0100

    Now that this are 'util' functions, they should be public.

--- src/main/clojure/com/kvardekdu/clinorm/util.clj
+++ src/main/clojure/com/kvardekdu/clinorm/util.clj
@@ -5,20 +5,20 @@
   "Throw an exception with the message msg."
   `(throw (new Exception ~msg)))
 
-(defn- str- 
+(defn str- 
   "Same as str, but keywords don't end up with a prepended ':'"
   [obj]
   (if (keyword? obj) (name obj) (str obj)))
 
-(defn- key- [key-value]
+(defn key- [key-value]
   "Return a stringified key on a key-value pair."
   (str- (first key-value)))
 
-(defn- value- [key-value]
+(defn value- [key-value]
   "Return a stringified value on a key-value pair."
   (str- (second key-value)))
 
-(defn- as-properties [properties]
+(defn as-properties [properties]
   "Given a map, return it as a Properties object."
   (let [properties-object (new Properties)]
     (doseq [property properties]
@@ -26,10 +26,10 @@
 		    (key- property) (value- property))
     properties-object)))
 
-(defn- str-concat [strs]
+(defn str-concat [strs]
   "Concatenate a sequence of strings."
   (reduce str strs))
 
-(defn- str-join [delimiter strs]
+(defn str-join [delimiter strs]
   "Join a sequence of strings separated by a delimiter ignoring empty strings."
   (str-concat (interpose delimiter (filter #(not (empty? %)) strs))))
