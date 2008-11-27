1e07b3206234185b26f20811be87f700e042909c
J. Pablo Fernández <pupeno@pupeno.com>
2008-11-27 23:14:00 +0100

    Succesfully create and drop a table of a model.

--- src/main/clojure/com/kvardekdu/clinorm.clj
+++ src/main/clojure/com/kvardekdu/clinorm.clj
@@ -1,12 +1,15 @@
-(ns com.kvardekdu.clinorm)
+(ns com.kvardekdu.clinorm
+  (:use com.kvardekdu.clinorm.util)
+  (:require [com.kvardekdu.clinorm.sql :as sql]))
 
-(defmacro defmodel [name & keys]
+(defmacro defmodel [name fields]
   (let [super-name (fn [template]
-		     (symbol (format template name)))
+		     (symbol (format template (str- name))))
 	create-table (super-name "create-table-%s")
 	drop-table (super-name "drop-table-%s")]
-    `(do
-       (defstruct ~name
-	 :id ~@(map first keys))
-       (defn ~create-table [])
-       (defn ~drop-table []))))
+    `[(defstruct ~(symbol (str- name))
+	  ~@(map first fields))
+	(defn ~create-table []
+	  (sql/create-table ~name ~fields))
+	(defn ~drop-table []
+	 (sql/drop-table ~name))]))
